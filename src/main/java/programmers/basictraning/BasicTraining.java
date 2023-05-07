50815d78de521a37af1a664074ec1b01dad9a076
JSY <tjdudd2@naver.com>
2023-05-07 23:49:02 +0900

    프로그래머스 '대소문자 바꿔서 출력하기' 완료 (https://school.programmers.co.kr/learn/courses/30/lessons/181949)

---
+++ src/main/java/programmers/basictraning/BasicTraining.java
@@ -0,0 +1,22 @@
+package programmers.basictraning;
+
+import org.junit.Test;
+
+public class BasicTraining {
+	@Test
+	// 대소문자 바꿔서 출력하기
+	public void _181949() {
+		String a = "aBcDeFg";
+		char[] strArr = a.toCharArray();
+
+		for (int i = 0; i < strArr.length; i++) {
+			if (strArr[i] >= 64 && strArr[i] <= 90) {			// 대문자
+				strArr[i] += 32;
+			} else if (strArr[i] >= 97 && strArr[i] <= 122) {	// 소문자
+				strArr[i] -= 32;
+			}
+		}
+
+		System.out.println(strArr);
+	}
+}
