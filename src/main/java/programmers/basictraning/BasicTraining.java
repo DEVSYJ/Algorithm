cdbd51b0f6f0b061c3d4e215a303492724fb552d
JSY <tjdudd2@naver.com>
2023-06-11 00:43:56 +0900

    프로그래머스 181943, 181942

--- src/main/java/programmers/basictraning/BasicTraining.java
+++ src/main/java/programmers/basictraning/BasicTraining.java
@@ -21,4 +21,35 @@ public class BasicTraining {
 
 		System.out.println(strArr);
 	}
+
+	@Test
+	void _181943() {
+		String my_string = "He11oWor1d";
+		String overwrite_string = "lloWorl";
+		int s = 2;
+
+		String answer = my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length());
+
+		assertThat(answer)
+				.isEqualTo("HelloWorld");
+	}
+
+	@Test
+	void _181942() {
+		String str1 = "aaaaa";
+		String str2 = "bbbbb";
+
+		StringBuilder sb = new StringBuilder();
+
+		char[] chars1 = str1.toCharArray();
+		char[] chars2 = str2.toCharArray();
+
+		for (int i = 0; i < str1.length(); i++) {
+			sb.append(chars1[i]);
+			sb.append(chars2[i]);
+		}
+
+		assertThat(sb.toString())
+				.isEqualTo("ababababab");
+	}
 }
